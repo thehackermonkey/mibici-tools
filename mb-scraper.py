6d98e68c68abc530117a6121fbd59fcd03c53d7b
Hans <hans@hackergarage.mx>
2016-05-01 15:13:51 -0500

    simulating json

--- mb-scraper.py
+++ mb-scraper.py
@@ -8,10 +8,37 @@ ss = bsObj.script.get_text()
 
 scrip_txt = StringIO(ss)
 
-def getmarks(script):
+# def getmarks(script, initstring):
+# 	for line in script:
+# 		if line.startswith(initstring):
+# 			elementos = line.split(",")
+# 			for elemento in elementos:
+# 				print(elemento+',')
+#getmarks(scrip_txt, 'newMarker')
+
+def getmarks(script, initstring):
 	for line in script:
-		if line.startswith('newMarker'):
-			print(line)
+		if line.startswith(initstring):
+			elementos = line.split(",")
+			splitid = elementos[3].split(")")
+			splitstatus = elementos[0].split("(")
+			
+
+			status = splitstatus[1].replace("'","")
+			lat = elementos[1]
+			lon = elementos[2]
+			idname = splitid[0].replace("'(", "")
+			stationdesc = splitid[1].replace("'(", "")
+			totalcapacity = elementos[4]
+			freespaces = elementos[5]
+
+			bikesplit =elementos[6].split(")")
+			bikes = bikesplit[0]
+
+			mark = {"status": status , "lat": float(lat) , "long": float(lon) , "id": idname , "station-desc": stationdesc , "total": int(totalcapacity) , "espacios": int(freespaces) , "bicicletas": int(bikes)}
+			print(mark)
+
+getmarks(scrip_txt, 'newMarker')
 
 
-getmarks(scrip_txt)
+#experiment area
